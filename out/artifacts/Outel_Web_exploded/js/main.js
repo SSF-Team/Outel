849ca140087fe132c384d4c76ed22b99d985fbd7
Stapx Steve <1007028430.stapx@gmail.com>
2020-12-24 19:16:37 +0800

    Cha | Move as Java Web Object

---
+++ out/artifacts/Outel_Web_exploded/js/main.js
@@ -0,0 +1,13 @@
+function inputChoice(nameBorder, nameTitle) {
+    var border=document.getElementById(nameBorder);
+    border.style.border = "2px solid #69ADFF";
+    var title=document.getElementById(nameTitle);
+    title.style.color = "#69ADFF";
+}
+
+function inputunChoice(nameBorder, nameTitle) {
+    var border=document.getElementById(nameBorder);
+    border.style.border = "2px solid rgba(0, 0, 0, 0.3)";
+    var title=document.getElementById(nameTitle);
+    title.style.color = "#000";
+}
